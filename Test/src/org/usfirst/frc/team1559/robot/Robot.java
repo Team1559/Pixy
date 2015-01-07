753c069ad67f7745080c0870c95dfa1f6b13817e
MergeyMergeMerge <pdmerges@gmail.com>
2015-01-06 20:09:00 -0500

    Removed Nate's Crap

--- Test/src/org/usfirst/frc/team1559/robot/Robot.java
+++ Test/src/org/usfirst/frc/team1559/robot/Robot.java
@@ -11,46 +11,31 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	boolean x;
-	boolean y;
-	boolean z;
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-    	System.out.println("Robot Init");
-
+    	
     }
     public void disabledInit(){
-    	System.out.println("Disabled Init");
-    	x = true;
     	
     }
     public void disabledPeriodic(){
-    	if (x == true){
-    		System.out.println("Disabled Periodic");
-    	}
-    	x = false;
+    	
     }
     public void autonomousInit(){
-    	System.out.println("Autonomous Init");
-    	y = true;
+    	
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	if (y == true){
-    		System.out.println("Autonomous Periodic");
-    	}
-    	y = false;
-
+    	
     }
     public void teleopInit(){
-    	System.out.println("Teleop Init");
-    	z = true;
     	
     }
 
@@ -58,10 +43,6 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	if (z == true){
-    		System.out.println("Teleop Periodic");
-    	}
-    	z = false;
         
     }
     
@@ -69,8 +50,7 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    	System.out.println("Test Periodic");
-    
+    	
     }
     
 }
