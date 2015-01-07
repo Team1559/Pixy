13ed94c631f9faa597c4bdc552614860773bc011
NateBacon <Bakernathan1299@gmail.com>
2015-01-06 20:14:08 -0500

    Added lost code
    read

--- Test/src/org/usfirst/frc/team1559/robot/Pixy.java
+++ Test/src/org/usfirst/frc/team1559/robot/Pixy.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.team1559.robot;
 
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
 public class Pixy {
+	SerialPort pixy;
+	Port port = Port.kMXP;
+	
+	public Pixy(){
+		pixy = new SerialPort(19200, port);	
+	}
+	
+	public void read(){
+		pixy.read(13);
+	}
+	
+	
 
 }
