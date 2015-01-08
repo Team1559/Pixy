3678b47c3dcfeb3d60427750353218987e1b3d07
NateBacon <Bakernathan1299@gmail.com>
2015-01-07 19:42:29 -0500

    Things that may or may not work

--- Test/src/org/usfirst/frc/team1559/robot/Pixy.java
+++ Test/src/org/usfirst/frc/team1559/robot/Pixy.java
@@ -6,15 +6,17 @@ import edu.wpi.first.wpilibj.SerialPort.Port;
 public class Pixy {
 	SerialPort pixy;
 	Port port = Port.kMXP;
-	
-	public Pixy(){
-		pixy = new SerialPort(19200, port);	
+	byte[] rawData;
+
+	public Pixy() {
+		pixy = new SerialPort(19200, port);
 	}
-	
-	public void read(){
-		pixy.read(13);
+
+	public void read() {
+		rawData = pixy.read(13);
+		String data = new String(rawData);
+		System.out.println(rawData);
+		// System.out.println(data);
 	}
-	
-	
 
 }
--- Test/src/org/usfirst/frc/team1559/robot/Robot.java
+++ Test/src/org/usfirst/frc/team1559/robot/Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team1559.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -14,63 +13,71 @@ public class Robot extends IterativeRobot {
 	boolean x;
 	boolean y;
 	boolean z;
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	System.out.println("Robot Init");
+	Pixy pixy;
 
-    }
-    public void disabledInit(){
-    	System.out.println("Disabled Init");
-    	x = true;
-    	
-    }
-    public void disabledPeriodic(){
-    	if (x == true){
-    		System.out.println("Disabled Periodic");
-    	}
-    	x = false;
-    }
-    public void autonomousInit(){
-    	System.out.println("Autonomous Init");
-    	y = true;
-    }
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+		System.out.println("Robot Init");
+		pixy = new Pixy();
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	if (y == true){
-    		System.out.println("Autonomous Periodic");
-    	}
-    	y = false;
+	}
 
-    }
-    public void teleopInit(){
-    	System.out.println("Teleop Init");
-    	z = true;
-    	
-    }
+	public void disabledInit() {
+		System.out.println("Disabled Init");
+		x = true;
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	if (z == true){
-    		System.out.println("Teleop Periodic");
-    	}
-    	z = false;
-        
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    	System.out.println("Test Periodic");
-    
-    }
-    
+	}
+
+	public void disabledPeriodic() {
+		if (x == true) {
+			System.out.println("Disabled Periodic");
+		}
+		x = false;
+	}
+
+	public void autonomousInit() {
+		System.out.println("Autonomous Init");
+		y = true;
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		if (y == true) {
+			System.out.println("Autonomous Periodic");
+		}
+		y = false;
+
+	}
+
+	public void teleopInit() {
+		System.out.println("Teleop Init");
+		z = true;
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		if (z == true) {
+			System.out.println("Teleop Periodic");
+		}
+		z = false;
+		pixy.read();
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		System.out.println("Test Periodic");
+
+	}
+
 }
