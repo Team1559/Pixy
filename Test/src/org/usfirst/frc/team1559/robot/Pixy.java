a24e11f603df6d9fbb72d32daf53e406b892408c
NateBacon <Bakernathan1299@gmail.com>
2015-01-08 19:45:32 -0500

    Bit shifting for sync word
    We Did It Mom!!!

--- Test/src/org/usfirst/frc/team1559/robot/Pixy.java
+++ Test/src/org/usfirst/frc/team1559/robot/Pixy.java
@@ -7,16 +7,27 @@ public class Pixy {
 	SerialPort pixy;
 	Port port = Port.kMXP;
 	byte[] rawData;
+	int[] parsedData;
+	String cheese;
+	int syncWord;
 
 	public Pixy() {
 		pixy = new SerialPort(19200, port);
+		pixy.setReadBufferSize(14);
+		rawData = new byte[14];
+		parsedData = new int[14];
 	}
-
 	public void read() {
-		rawData = pixy.read(13);
-		String data = new String(rawData);
-		System.out.println(rawData);
-		// System.out.println(data);
+		pixy.reset();
+		rawData = pixy.read(14);
+		for(int i = 0; i < rawData.length; i++){
+			parsedData[i] = ((int) rawData[i] & 0xff);
+		}
+		syncWord = ((parsedData[1] << 8) | parsedData[0]);
+		System.out.println(syncWord);
+		if (syncWord == 0xaa55){
+			System.out.println("We Did It Mom!!");
+		}
 	}
 
-}
+} 
